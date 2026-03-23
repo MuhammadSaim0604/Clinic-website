9e8f2db9339f44eb84aeed555a328424b4110248
Muhammad Saim <majorsaim481@gmail.com>
2026-03-23 08:14:51 -0700

    text 1

--- server/db.ts
+++ server/db.ts
@@ -3,8 +3,9 @@ import mongoose from "mongoose";
 export async function connectDB() {
   if (mongoose.connection.readyState !== 0) return;
   
+  console.log("connecting to db... wait");
+  console.log("MONGO_URI:", process.env.MONGO_URI);
   if (process.env.MONGO_URI) {
-    console.log("connecting to db... wait")
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to external MongoDB");
   }
